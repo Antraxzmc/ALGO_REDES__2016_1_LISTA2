4a6d82271f2362ec6c5d505d1adb7bb7b8766aeb
Gabriel de Assis <antraxzmc@gmail.com>
2016-02-26 21:01:45 -0300

    Added files via upload
    exercicios lista 2

---
+++ Lista Atividade 2/mes_do_ano.py
@@ -0,0 +1,29 @@
+mes = int(input("Coloque o número de acordo com a data do ano (1 - 12): "))
+if mes == 1:
+ print ('O mês de acordo com o numero é Janeiro')
+if mes == 2:
+ print ('O mês de acordo com o numero é Fevereiro')
+if mes == 3:
+ print ('O mês de acordo com o numero é Março')
+if mes == 4:
+ print ('O mês de acordo com o numero é Abril')
+if mes == 5:
+ print ('O mês de acordo com o numero é Maio')
+if mes == 6:
+ print ('O mês de acordo com o numero é Junho')
+if mes == 7:
+ print ('O mês de acordo com o numero é Julho')
+if mes == 8:
+ print ('O mês de acordo com o numero é Agosto')
+if mes == 9:
+ print ('O mês de acordo com o numero é Setembro')
+if mes == 10:
+ print ('O mês de acordo com o numero é Outubro')
+if mes == 11:
+ print ('O mês de acordo com o numero é Novembro')
+if mes == 12:
+ print ('O mês de acordo com o numero é Dezembro')
+if mes >=13:
+ print ('Por favor coloque número entre 1-12')
+if mes <=0:
+ print ('Por favor coloque número entre 1-12')
---
+++ Lista Atividade 2/salario.py
@@ -0,0 +1,4 @@
+nome = input("Coloque seu nome : ")
+salario = float(input("Coloque seu Salário : "))
+conta = float(salario) + float(salario) * 0.25
+print ("Senhor , seu novo salário é de %d" %(float(conta))) 
